868b45bf7a4968b915a253095b9cf3e0e2acc34b
hlaramelo <hlaramelo@gmail.com>
2016-02-01 22:18:24 +0000

    Done.

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,1 +1,11 @@
-# code your #position_taken? method here!
+def position_taken?(board, position)
+  if board[position] == " " || board[position] == "" || board[position] == nil
+    false
+  else if board[position] == "X" || board[position] == "O"
+    true
+  end
+  end
+end
+
+
+  # code your #position_taken? method here!
